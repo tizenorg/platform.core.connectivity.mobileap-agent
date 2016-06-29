00be2db054e21717329bf39d82a27f72c28761df
Seonah Moon <seonah1.moon@samsung.com>
2016-06-29 11:37:48 +0900

    Add default path to shell script
    Change-Id: I83b0a49b9cec220bcc2c0885cb9c035847653790
    Signed-off-by: Seonah Moon <seonah1.moon@samsung.com>

--- packaging/mobileap-agent.spec
+++ packaging/mobileap-agent.spec
@@ -1,6 +1,6 @@
 Name:		mobileap-agent
 Summary:	Mobile AP daemon for setting tethering environments
-Version:	1.0.60
+Version:	1.0.61
 Release:	1
 Group:		System/Network
 License:	Apache-2.0
--- tethering_dump.sh
+++ tethering_dump.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+PATH=/bin:/usr/bin:/sbin:/usr/sbin
 #
 # Tethering module log dump (tethering_dump.sh)
 #
@@ -8,10 +9,7 @@ TETH_DEBUG=$1/tethering
 mkdir -p ${TETH_DEBUG}
 
 # copy files
-#/bin/cp -a /tmp/hostapd.log ${TETH_DEBUG}
 /bin/cp -rf /tmp/dnsmasq.conf /opt/var/lib/misc/* ${TETH_DEBUG}
 
-#vconftool get db/mobile_hotspot > ${TETH_DEBUG}/vconf_db.log
-#vconftool get memory/mobile_hotspot > ${TETH_DEBUG}/vconf_memory.log
 /usr/sbin/iptables -t nat -L -vv > ${TETH_DEBUG}/iptables_nat.log
 /usr/sbin/iptables -L -vv > ${TETH_DEBUG}/iptables_filter.log
